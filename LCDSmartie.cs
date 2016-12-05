1788b1f348d382bcb558538bb6683cabab5d2f48
torlye <foo@bar.com>
2016-12-05 17:46:28 +0100

    Update copyright notices

--- LCDSmartie.cs
+++ LCDSmartie.cs
@@ -12,7 +12,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-       Copyright Duncan Grieve, August 2008
+       Original work Copyright Duncan Grieve, August 2008
+       Modified work Copyright 2012 Tor Lye
 */
 using System;
 using System.IO;
